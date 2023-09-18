8d3cf72591ef7f912eb3df71aa914ac78a56e181
Teabenzoni <t.benzoni@studenti.unibg.it>
2023-09-18 11:35:48 +0200

    modifica prodotto

--- Pasticceria/src/model/Prodotto.java
+++ Pasticceria/src/model/Prodotto.java
@@ -1,20 +1,16 @@
 
 package model;
 
-import java.util.ArrayList;
-
 public class Prodotto {
 
 	public String IDprodotto;
 	public String Nome;
 	public float Prezzo;
-	public ArrayList<String> Ingredienti; // da controllare ( sbagliato)
 
-	public Prodotto(String ID, String nome, float prezzo, ArrayList<String> Ingr) {
+	public Prodotto(String ID, String nome, float prezzo) {
 		this.IDprodotto = ID;
 		this.Nome = nome;
 		this.Prezzo = prezzo;
-		this.Ingredienti = Ingr;
 	}
 
 	public String getIDprodotto() {
@@ -29,10 +25,6 @@ public class Prodotto {
 		return Prezzo;
 	}
 
-	public ArrayList<String> getIngredienti() {
-		return Ingredienti;
-	}
-
 	public void setIDprodotto(String IDprodotto) {
 		this.IDprodotto = IDprodotto;
 	}
@@ -43,11 +35,6 @@ public class Prodotto {
 
 	public void setPrezzo(float prezzo) {
 		this.Prezzo = prezzo;
-	}
-
-	public ArrayList<String> setIngredienti(String Ing) { // non posso usarlo per creare il prodotto
-		Ingredienti.add(Ing);
-		return Ingredienti;
 	}
 
 }
