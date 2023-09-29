b65e2b6f694acdb94bfeaafc8593d6bacbdc6ea1
a.marmo <a.marmo@studenti.unibg.it>
2023-09-29 12:30:38 +0200

    Inizio implemtazione interfaccia Home

--- Pasticceria/src/view/HomeIF.java
+++ Pasticceria/src/view/HomeIF.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.Font;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,57 +11,42 @@ import javax.swing.border.EmptyBorder;
 
 public class HomeIF extends JFrame {
 
-	private JPanel HomeContentPane; // dischiarazione variabili
-	private JButton prodottiButton;
-	private JButton visualizzaOrdineButton;
-	private JButton newOrdineButton;
+	private JPanel HomeContentPane;
+	private JButton NuovoOrdineButton;
 
-	/**
-	 * Launch the application.
-	 */
-	/*
-	 * public static void main(String[] args) { EventQueue.invokeLater(new
-	 * Runnable() { public void run() { try { MenuIF frame = new MenuIF();
-	 * frame.setVisible(true); } catch (Exception e) { e.printStackTrace(); } } });
-	 * }
-	 */
 	/**
 	 * Create the frame.
 	 */
-	public HomeIF(String utente) { // interfaccia Home //costruttore per l'aspetto grafico
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // chiusura interfaccia
-		setBounds(100, 100, 450, 300); // dimensioni e layout finestra
+	public HomeIF(String utente) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
 		setLocationRelativeTo(null);
 		setResizable(false);
-		HomeContentPane = new JPanel(); // contenitore delle componenti
+		HomeContentPane = new JPanel();
 		HomeContentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(HomeContentPane);
 		HomeContentPane.setLayout(null);
 
-		JLabel homeLabel = new JLabel("HOME "); // nome schermata
-		homeLabel.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		homeLabel.setBounds(182, 11, 114, 48);
-		HomeContentPane.add(homeLabel);
+		JLabel HomeLabel = new JLabel("HOME");
+		HomeLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		HomeLabel.setBounds(176, 11, 79, 39);
+		HomeContentPane.add(HomeLabel);
 
-		JLabel utenteLabel = new JLabel("Utente: "); // etichetta utente
-		utenteLabel.setBounds(10, 11, 46, 14);
-		HomeContentPane.add(utenteLabel);
+		JLabel UserLabel = new JLabel("User: ");
+		UserLabel.setBounds(10, 11, 41, 14);
+		HomeContentPane.add(UserLabel);
 
-		JLabel adminLabel = new JLabel(utente); // visualizzare nome utente nella schermata
-		adminLabel.setBounds(55, 11, 65, 14);
-		HomeContentPane.add(adminLabel);
+		JLabel UserInLabel = new JLabel(utente);
+		UserInLabel.setBounds(46, 11, 72, 14);
+		HomeContentPane.add(UserInLabel);
 
-		prodottiButton = new JButton("Visualizza Prodotti"); // Nome bottone
-		prodottiButton.setBounds(129, 54, 176, 23);
-		HomeContentPane.add(prodottiButton);
+		NuovoOrdineButton = new JButton("Nuovo Ordine");
+		NuovoOrdineButton.setBounds(151, 72, 117, 23);
+		HomeContentPane.add(NuovoOrdineButton);
+	}
 
-		visualizzaOrdineButton = new JButton("Visualizza Ordine"); // Nome bottone
-		visualizzaOrdineButton.setBounds(129, 126, 176, 23);
-		HomeContentPane.add(visualizzaOrdineButton);
-
-		newOrdineButton = new JButton("Nuovo Ordine"); // Nome bottone
-		newOrdineButton.setBounds(129, 88, 176, 23);
-		HomeContentPane.add(newOrdineButton);
+	public void openNuovoOrdineIF(ActionListener listener) {
+		NuovoOrdineButton.addActionListener(listener);
 	}
 }
