afb227906b66b6f66ce7b40c8718f52f45e219a5
a.marmo <a.marmo@studenti.unibg.it>
2023-10-10 12:23:28 +0200

    Rimozioni di classi non utilizzate

--- Pasticceria/src/model/StatoOrdine.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum StatoOrdine {
-	ACCETTATO, LAVORAZIONE, TERMINATO, ATTESA;
-}
--- Pasticceria/src/model/TipoOrdine.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum TipoOrdine { // identifica la tipologia del prodotto nel caso in cui possa essere modificato
-	STANDARD, PERSONALIZZATO;
-}
