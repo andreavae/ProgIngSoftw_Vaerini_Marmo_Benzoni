9b5b778289cac1fa179d36fd4ba31fa12b4e5e0e
andreavae <77801782+andreavae@users.noreply.github.com>
2023-10-13 16:25:09 +0200

    LoginTest aggiunte, sistemato flag

--- Pasticceria/src/testing/LoginTest.java
+++ Pasticceria/src/testing/LoginTest.java
@@ -11,29 +11,36 @@ class LoginTest {
 	@Test
 	void testisLoginValid() {
 		User utente = new User("admin", "admin");
-		boolean flag = utente.isLoginValid();
-		assertEquals(true, flag);
+		boolean flagValid = utente.isLoginValid();
+		assertEquals(true, flagValid);
 	}
 
 	@Test
 	void testisVipUSer() {
 		User utente = new User("admin", "admin");
-		boolean flagvip = utente.isVipUser("admin");
-		assertEquals(true, flagvip);
+		boolean flagvipUser = utente.isVipUser("admin");
+		assertEquals(true, flagvipUser);
 	}
 
 	@Test
 	void testisVipLogin() {
 		User utente = new User("admin", "admin");
-		boolean flagvip = utente.isVipLoginValid();
-		assertEquals(true, flagvip);
+		boolean flagvipLogin = utente.isVipLoginValid();
+		assertEquals(true, flagvipLogin);
 	}
 
 	@Test
 	void testisSignValid() {
 		User utente = new User("cliente", "cliente");
-		boolean flagsign = utente.isSignValid();
-		assertEquals(true, flagsign);
+		boolean flagsignValid = utente.isSignValid();
+		assertEquals(true, flagsignValid);
+	}
+
+	@Test
+	void testisSignVip() {
+		User utente = new User("admin", "admin");
+		boolean flagsignVip = utente.isSignVip("admin", 1);
+		assertEquals(true, flagsignVip);
 	}
 
 	@Test
