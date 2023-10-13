4d5a1f007145c5c8c3ba7361dfb18b90abf1ff38
andreavae <77801782+andreavae@users.noreply.github.com>
2023-10-13 13:05:07 +0200

    Classe PagamentoTest

--- Pasticceria/src/testing/PagamentoTest.java
+++ Pasticceria/src/testing/PagamentoTest.java
@@ -1,14 +1,67 @@
 package testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class PagamentoTest {
+import model.Ordine;
+import model.Pagamento;
+import model.User;
+
+public class PagamentoTest {
+	private Pagamento pagamento;
+	private Ordine ordine;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testOrdineValido() {
+		User utente = new User("admin", "admin");
+		Ordine ordine = new Ordine(utente);
+		boolean flagOrdine = ordine.isOrderValid(ordine, utente, 12);
+		assertEquals(true, flagOrdine);
+	}
+
+	@Test
+	public void testProcessoPagamentoValido() {
+		User utente = new User("admin", "admin");
+		Ordine ordine = new Ordine(utente);
+		String numeroCarta = "1234567890123456";
+		String dataScadenza = "12/23";
+		Pagamento pagamento = new Pagamento(ordine, numeroCarta, dataScadenza);
+		boolean flagPagamento = pagamento.processoPagamento(numeroCarta, dataScadenza, 1000);
+		assertEquals(true, flagPagamento);
+	}
+
+	@Test
+	public void testProcessoPagamentoInvalido_NumeroCartaNonValido() {
+		User utente = new User("admin", "admin");
+		Ordine ordine = new Ordine(utente);
+		String numeroCarta = "123456789012345";
+		String dataScadenza = "12/23";
+		Pagamento pagamento = new Pagamento(ordine, numeroCarta, dataScadenza);
+		boolean flagPagamento = pagamento.processoPagamento(numeroCarta, dataScadenza, 1000);
+		assertEquals(false, flagPagamento);
+	}
+
+	@Test
+	public void testProcessoPagamentoInvalido_DataScadenzaNonValida() {
+		User utente = new User("admin", "admin");
+		Ordine ordine = new Ordine(utente);
+		String numeroCarta = "123456789012346";
+		String dataScadenza = "12-23";
+		Pagamento pagamento = new Pagamento(ordine, numeroCarta, dataScadenza);
+		boolean flagPagamento = pagamento.processoPagamento(numeroCarta, dataScadenza, 1000);
+		assertEquals(false, flagPagamento);
+
+	}
+
+	@Test
+	public void testGetNumeroCarta() {
+		User utente = new User("admin", "admin");
+		Ordine ordine = new Ordine(utente);
+		String numeroCarta = "1234567890123456";
+		String dataScadenza = "12/23";
+		Pagamento pagamento = new Pagamento(ordine, numeroCarta, dataScadenza);
+		assertEquals("1234567890123456", pagamento.getNumeroCarta());
 	}
 
 }
