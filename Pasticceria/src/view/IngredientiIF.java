e651725c162e2cfe6e76f8883efb40a8423da7ed
Teabenzoni <t.benzoni@studenti.unibg.it>
2023-10-14 22:32:49 +0200

    Inserimento vista ingredienti

---
+++ Pasticceria/src/view/IngredientiIF.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class IngredientiIF extends JFrame {
+	private JPanel contentPane;
+	public JButton BackButton;
+
+}
