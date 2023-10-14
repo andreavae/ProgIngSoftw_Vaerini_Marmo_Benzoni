3f10efa6cf9b303e3e4b2b29060bb3325e5dc10d
Teabenzoni <t.benzoni@studenti.unibg.it>
2023-10-14 22:33:59 +0200

    implementazione bottone back

--- Pasticceria/src/view/IngredientiIF.java
+++ Pasticceria/src/view/IngredientiIF.java
@@ -1,11 +1,36 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class IngredientiIF extends JFrame {
 	private JPanel contentPane;
 	public JButton BackButton;
+
+	public IngredientiIF() {
+		// chiusura interfaccia
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(900, 440); // grandezza finestra
+		setLocationRelativeTo(null); // posizione della finsetra al centro
+		setResizable(false); // impedisce all'utente di ridimensionare la finestra
+		contentPane = new JPanel(); // per aggiungere componenti grafici
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(new Color(255, 180, 180));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		BackButton = new JButton("Back"); // bottone back
+		BackButton.setForeground(new Color(255, 255, 255));
+		BackButton.setBackground(new Color(255, 80, 120));
+		BackButton.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
+		BackButton.setBounds(30, 350, 89, 23);
+		contentPane.add(BackButton);
+
+	}
 
 }
