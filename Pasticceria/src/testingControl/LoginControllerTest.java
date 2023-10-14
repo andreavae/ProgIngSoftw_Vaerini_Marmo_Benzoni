d076a047a79b6ca5ff98087ab315ea87a860064e
a.marmo <a.marmo@studenti.unibg.it>
2023-10-14 16:52:22 +0200

    Implementazione di test da correggere

--- Pasticceria/src/testingControl/LoginControllerTest.java
+++ Pasticceria/src/testingControl/LoginControllerTest.java
@@ -1,75 +1,47 @@
 package testingControl;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import java.awt.event.ActionEvent;
-
-import javax.swing.JRadioButton;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 import control.LoginController;
 import model.User;
 import view.LoginIF;
 
 public class LoginControllerTest {
-
-	@Mock
-	private LoginIF loginif;
-	private User user;
 	private LoginController loginController;
+	private LoginIF loginIf;
+	private User user;
 
 	@Before
 	public void setUp() {
-		MockitoAnnotations.initMocks(this); // Inizializza i mock
-
-		// Crea un utente fittizio per i test
-		user = new User("cliente", "cliente");
-
-		loginController = new LoginController(loginif, user);
+		loginIf = mock(LoginIF.class);
+		user = new User("admin", "admin");
+		user = mock(User.class);
+		loginController = new LoginController(loginIf, user);
 	}
 
 	@Test
-    public void testLoginListenerActionPerformed_LoginValid() {
-        // Simula il comportamento dell'utente in modo che ritorni true quando si chiama il metodo isLoginValid().
-        when(loginif.getClientePremiumRadioButton()).thenReturn(Mockito.mock(JRadioButton.class));
-        when(loginif.getClientePremiumRadioButton().isSelected()).thenReturn(false);
-        when(user.isLoginValid()).thenReturn(true);
+    public void testLoginListenerValid() {
+        when(loginIf.getUsernameField()).thenReturn("admin");
+        when(loginIf.getPasswordField()).thenReturn("admin");
+        when(loginIf.getClientePremiumRadioButton().isSelected()).thenReturn(false);
+        when(loginIf.getClientePremiumRadioButton().isSelected()).thenReturn(false);
 
-        // Crea un evento fittizio per il pulsante di login
-        ActionEvent actionEvent = new ActionEvent(new Object(), ActionEvent.ACTION_PERFORMED, "Login");
+        loginController.new LoginListener().actionPerformed(null);
 
-        // Esegui l'azione sull'ascoltatore "LoginListener"
-        loginController.new LoginListener().actionPerformed(actionEvent);
-
-        // Verifica che sia stata creata un'istanza di HomeIF
-     //   assertNotNull(loginif.getHomeIF());
-
-        // Verifica che sia stato creato un HomeController
-       // assertNotNull(loginif.getHomeController());
+        // Assert the expected behavior here
     }
-
-	@Test
-    public void testLoginListenerActionPerformed_LoginInvalid() {
-        // Simula il comportamento dell'utente in modo che ritorni false quando si chiama il metodo isLoginValid().
-        when(loginif.getClientePremiumRadioButton()).thenReturn(Mockito.mock(JRadioButton.class));
-        when(loginif.getClientePremiumRadioButton().isSelected()).thenReturn(false);
-        when(user.isLoginValid()).thenReturn(false);
-
-        // Crea un evento fittizio per il pulsante di login
-        ActionEvent actionEvent = new ActionEvent(new Object(), ActionEvent.ACTION_PERFORMED, "Login");
-
-        // Esegui l'azione sull'ascoltatore "LoginListener"
-        loginController.new LoginListener().actionPerformed(actionEvent);
-
-        // Verifica che non sia stata creata un'istanza di HomeIF
-      //  assertNull(loginif.getHomeIF());
-
-        // Verifica che non sia stato creato un HomeController
-       // assertNull(loginif.getHomeController());
-    }
+	/*
+	 * @Test public void testLoginListenerInvalid() {
+	 * when(loginIf.getUsernameField()).thenReturn("invalidUsername");
+	 * when(loginIf.getPasswordField()).thenReturn("invalidPassword");
+	 * when(loginIf.getClientePremiumRadioButton().isSelected()).thenReturn(false);
+	 * 
+	 * loginController.new LoginListener().actionPerformed(null);
+	 * 
+	 * // Assert the expected behavior here }
+	 */
 }
--- Pasticceria/src/testingView/VisualizzaOrdineIFTest.java
+++ Pasticceria/src/testingView/VisualizzaOrdineIFTest.java
@@ -1,36 +1,64 @@
 package testingView;
 
-import static org.junit.Assert.assertTrue;
-
+import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JButtonFixture;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import model.OrdineStandard;
+import model.User;
+import view.LoginIF;
 import view.VisualizzaOrdineIF;
 
 public class VisualizzaOrdineIFTest {
-
-	private VisualizzaOrdineIF visualizzaOrdineFrame;
+	private FrameFixture window;
+	private LoginIF loginif;
+	private User utente;
+	private OrdineStandard ordinestandard;
 
 	@Before
 	public void setUp() {
-		visualizzaOrdineFrame = new VisualizzaOrdineIF(null, null, null, 0);
+		// Crea l'istanza di VisualizzaOrdineIF da testare
+		loginif = new LoginIF();
+		utente = new User("admin", "admin");
+		ordinestandard = new OrdineStandard(utente, 0);
+		VisualizzaOrdineIF visualizzaOrdineIF = new VisualizzaOrdineIF(loginif, utente, ordinestandard, 0);
+		// Imposta la finestra di test su quella di VisualizzaOrdineIF
+		window = new FrameFixture(visualizzaOrdineIF);
+		window.show(); // Mostra la finestra
+	}
+
+	@After
+	public void tearDown() {
+		// Chiudi la finestra alla fine del test
+		window.cleanUp();
 	}
 
 	@Test
-	public void testBackButton() {
-		// Verifica che il pulsante "Back" sia abilitato
-		assertTrue(visualizzaOrdineFrame.BackButton.isEnabled());
+	public void testBackButtonListener() {
+		// Clicca sul pulsante "Back"
+		JButtonFixture backButton = window.button("BackButton");
+		backButton.click();
+
+		// Aggiungi le verifiche necessarie qui
 	}
 
 	@Test
-	public void testConfermaOrdineButton() {
-		// Verifica che il pulsante "Conferma Ordine" sia abilitato
-		assertTrue(visualizzaOrdineFrame.ConfermaOrdineButton.isEnabled());
+	public void testConfermaOrdineButtonListener() {
+		// Clicca sul pulsante "Conferma Ordine"
+		JButtonFixture confermaButton = window.button("ConfermaOrdineButton");
+		confermaButton.click();
+
+		// Aggiungi le verifiche necessarie qui
 	}
 
 	@Test
-	public void testCancellaButton() {
-		// Verifica che il pulsante "Cancella" sia abilitato
-		assertTrue(visualizzaOrdineFrame.CancellaButton.isEnabled());
+	public void testCancellaButtonListener() {
+		// Clicca sul pulsante "Cancella"
+		JButtonFixture cancellaButton = window.button("CancellaButton");
+		cancellaButton.click();
+
+		// Aggiungi le verifiche necessarie qui
 	}
 }
