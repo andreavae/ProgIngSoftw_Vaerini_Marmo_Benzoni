a90f47ff4604de39a730e7c597b331ef50640974
Teabenzoni <t.benzoni@studenti.unibg.it>
2023-10-14 22:25:58 +0200

    Ingredienti Controller

--- Pasticceria/src/control/IngredientiController.java
+++ Pasticceria/src/control/IngredientiController.java
@@ -1,5 +1,31 @@
 package control;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.IngredientiIF;
+import view.OrdineStandardIF;
+
 public class IngredientiController {
+
+	private IngredientiIF ingredientiif;
+	private OrdineStandardIF ordinestandardif;
+
+	public IngredientiController(IngredientiIF ingredientiif, OrdineStandardIF ordinestandardif) {
+		this.ingredientiif = ingredientiif;
+		this.ordinestandardif = ordinestandardif;
+		this.ingredientiif.back(new back());
+
+	}
+
+	// gestione pulsante back
+	public class back implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// visibilit� interfacce
+			ordinestandardif.setVisible(true);
+			ingredientiif.dispose();
+		}
+	}
 
 }
