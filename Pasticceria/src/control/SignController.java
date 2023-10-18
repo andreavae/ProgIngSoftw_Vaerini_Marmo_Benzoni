4d623ae69a1cddde5a18d19f0fee5920e9f0b206
a.marmo <a.marmo@studenti.unibg.it>
2023-10-18 11:40:36 +0200

    Messaggio per l'utente

--- Pasticceria/src/control/SignController.java
+++ Pasticceria/src/control/SignController.java
@@ -38,7 +38,7 @@ public class SignController { // dichiarazione variabili
 				loginif.setVisible(true);
 				LoginController controller = new LoginController(loginif, user);
 			} else {
-				JOptionPane.showMessageDialog(signif, "User giù utilizzato! Riprovare o Registrati.");
+				JOptionPane.showMessageDialog(signif, "User giù utilizzato.!");
 			}
 		}
 	}
